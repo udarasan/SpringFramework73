68083ead0f2cbee26d63f6389ee69c19672660cf
Udara San <udarassanjeewa@gmail.com>
2026-01-24 14:03:12 +0530

    import annotation

--- 05_Configurations/src/main/java/org/example/config/AppConfig.java
+++ 05_Configurations/src/main/java/org/example/config/AppConfig.java
@@ -3,10 +3,13 @@ package org.example.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ComponentScan(basePackages = "org.example.bean")
 @Import({AppConfigOne.class,AppConfigTwo.class})
+@ImportResource("classpath:hibernate.cfg.xml")
+//@ImportResource("file:C:/config/hibernate.cfg.xml")
 public class AppConfig {
     public AppConfig() {
         System.out.println("AppConfig Constructor");

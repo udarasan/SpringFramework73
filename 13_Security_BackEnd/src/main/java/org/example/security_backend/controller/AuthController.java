9a8faa474940af98c9733eccae06a06f9e99ac8f
Udara San <udarassanjeewa@gmail.com>
2026-02-19 15:28:11 +0530

    jwt updated

---
+++ 13_Security_BackEnd/src/main/java/org/example/security_backend/controller/AuthController.java
@@ -0,0 +1,29 @@
+package org.example.security_backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.security_backend.dto.APIResponse;
+import org.example.security_backend.dto.AuthDTO;
+import org.example.security_backend.dto.RegisterDTO;
+import org.example.security_backend.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/auth")
+@CrossOrigin
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+    @PostMapping("register")
+    public ResponseEntity<APIResponse> registerUser(@RequestBody RegisterDTO registerDTO) {
+        return ResponseEntity.ok(new APIResponse
+                (200,"OK",authService.register(registerDTO)));
+    }
+    @PostMapping("login")
+    public ResponseEntity<APIResponse> loginUser(@RequestBody AuthDTO authDTO) {
+        return ResponseEntity.ok(new APIResponse(
+                200,"OK",authService.authenticate(authDTO)
+        ));
+    }
+
+}
